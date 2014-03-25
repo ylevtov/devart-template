f55517d49ea409c3d2865a6de31537d6a273b54c
Owen Hindley <owenhindley@hotmail.com>
2014-03-25 12:31:16 +0000

    changes to client card class

--- project_code/client/js/DynamicsCardClient.js
+++ project_code/client/js/DynamicsCardClient.js
@@ -55,9 +55,18 @@
 
 		}
 
+
+
 		this.element.appendChild(this.controlElement);
+		
 
 		aCardContainerElement.appendChild(this.element);
+		
+		setTimeout(function() {
+			this.element.className = "card active";
+		}.bind(this), 100)
+
+		console.log("added card : " + this.name);
 
 	}
 
@@ -66,16 +75,21 @@
 		this.order--;
 
 		this.element.style.marginTop = this.order * 20 + "px";
-		this.element.style.opacity = Math.max(1 + (this.order / 10), 0);
+		this.element.style.webkitTransform = "scale(" + (1 + (this.order * 0.05)) + ")";
+		this.element.style.opacity = Math.max(1 + (this.order / 5), 0);
 
 	};
 
 	p.increaseOrder = function() {
 
 		this.order++;
+		
+		if (this.order > 0) this.order = 0;
+
 
 		this.element.style.marginTop = this.order * 20 + "px";
-		this.element.style.opacity = Math.max(1 + (this.order / 10), 0);
+		this.element.style.webkitTransform = "scale(" + (1 + (this.order * 0.05)) + ")";
+		this.element.style.opacity = Math.max(1 + (this.order / 5), 0);
 
 	}
 
@@ -90,22 +104,24 @@
 	};
 
 
-	p._onCardExpire = function() {
+	p._onCardExpire = function(aData) {
 
-		console.log("DynamicsCardClient :: Card " + this.name + " expired");
+		if (aData == this.name){
+			console.log("DynamicsCardClient :: Card " + this.name + " expired");
 
-		this.destroy();
-
+			this.destroy();
+		}
 	};
 
 
 	p.destroy = function() {
 
-		$(this.element).fadeOut();
+		this.element.style.opacity = 0;
 
 		setTimeout(function() {
 
-			this.element.parentElement.removeChild(this.element);
+			if (this.element.parentElement)
+				this.element.parentElement.removeChild(this.element);
 
 		}.bind(this), 2000);
 
